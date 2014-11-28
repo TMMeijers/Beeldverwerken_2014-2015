40cd93e80d805d421889fc6c85d8ca013afc3975
TMMeijers <t.m.meijers@Hotmail.com>
2014-11-28 17:29:44 +0100

    Finished section 3, ransac to do!

--- P03/main.m
+++ P03/main.m
@@ -14,6 +14,7 @@
 vl_version verbose
 
 clear
+
 %% Section 2: Projectivity
 
 % Run the premade demo
@@ -22,20 +23,104 @@ demo_mosaic;
 clear
 %% 2.1: Projectivity Matrix
 
+% Run demo with points specified (argument)
 demo_mosaic_npoints(10);
 
 clear;
 
-%% Section 3: SIFT
+%% Section 3: Using vl_ubcmatch 
+% "Use vl_feat to find matches for the Nachtwacht images. Visualize the
+% matches."
 
-img1 = imread('nachtwacht1.jpg');
-img1 = rgb2gray(img1);
+
+% Load image
+nachtwacht1 = imread('nachtwacht1.jpg');
+img1 = rgb2gray(nachtwacht1);
 img1 = im2single(img1);
-img2 = imread('nachtwacht2.jpg');
-img2 = rgb2gray(img2);
+nachtwacht2 = imread('nachtwacht2.jpg');
+img2 = rgb2gray(nachtwacht2);
 img2 = im2single(img2);
 
-data = vl_shift(img1);
-data2 = vl_shift(img2);
+% Get descriptors and features, find matches
+[F1, D1] = vl_sift(img1);
+[F2, D2] = vl_sift(img2);
+matches = vl_ubcmatch(D1, D2);
 
-features = vl_ubcmatch(data, data2);
+% Get coordinates
+m1 = matches(1,:);
+m1coords = F1(:,m1);
+m1coords = m1coords([1:2],:);
+m2 = matches(2,:);
+m2coords = F2(:,m2);
+m2coords = m2coords([1:2],:);
+
+% Plot images and matching points
+figure('name','Visualizing matches');
+subplot(1, 2, 1);
+imshow(nachtwacht1);
+for i = 1:length(m1coords)
+    text(m1coords(1, i), m1coords(2,i), sprintf('%02d',i), 'Color', 'red');
+end
+title('Nachtwacht1.jpg');
+
+subplot(1, 2, 2);
+imshow(nachtwacht2);
+for i = 1:length(m2coords)
+    text(m2coords(1, i), m2coords(2,i), sprintf('%02d',i), 'Color', 'red');
+end
+title('Nachtwacht2.jpg');
+
+clear;
+
+%% Section 3: Projection matrix to predict matches
+
+% Load image
+nachtwacht1 = imread('nachtwacht1.jpg');
+img1 = rgb2gray(nachtwacht1);
+img1 = im2single(img1);
+nachtwacht2 = imread('nachtwacht2.jpg');
+img2 = rgb2gray(nachtwacht2);
+img2 = im2single(img2);
+
+% Get descriptors and features, find matches
+[F1, D1] = vl_sift(img1);
+[F2, D2] = vl_sift(img2);
+matches = vl_ubcmatch(D1, D2);
+
+% Get coordinates
+m1 = matches(1,:);
+m1coords = F1(:,m1);
+m1coords = m1coords([1:2],:);
+m2 = matches(2,:);
+m2coords = F2(:,m2);
+m2coords = m2coords([1:2],:);
+
+% Get 4 points and make projection matrix
+pcoords1 = m1coords(:,[2:5])'; % 2 to 5 are correct matches
+pcoords2 = m2coords(:,[2:5])';
+P = projectionMatrix(pcoords1, pcoords2);
+
+% Transform to real coordinates and drop 3rd dimension (1's)
+m2coords = P * [m1coords; ones(1, length(m1coords))];
+for i = 1:length(m2coords)
+    m2coords(:,i) = m2coords(:,i) ./ m2coords(3,i);
+end
+m2coords = m2coords([1:2],:);
+
+% Plot images and matching points
+figure('name','Visualizing own matches');
+subplot(1, 2, 1);
+imshow(nachtwacht1);
+for i = 1:length(m1coords)
+    text(m1coords(1, i), m1coords(2,i), sprintf('%02d',i), 'Color', 'red');
+end
+title('Nachtwacht1.jpg');
+
+subplot(1, 2, 2);
+imshow(nachtwacht2);
+for i = 1:length(m2coords)
+    text(m2coords(1, i), m2coords(2,i), sprintf('%02d',i), 'Color', 'red');
+end
+title('Nachtwacht2.jpg');
+
+clear;
