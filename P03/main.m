a9325b78f57cddecb30143ff173c93c5be8422b9
TMMeijers <t.m.meijers@Hotmail.com>
2014-11-28 15:22:30 +0100

    updated pdfs, more code skeletons

--- P03/main.m
+++ P03/main.m
@@ -22,8 +22,20 @@ demo_mosaic;
 clear
 %% 2.1: Projectivity Matrix
 
-demo_mosaic_npoints(6);
+demo_mosaic_npoints(10);
 
 clear;
 
 %% Section 3: SIFT
+
+img1 = imread('nachtwacht1.jpg');
+img1 = rgb2gray(img1);
+img1 = im2single(img1);
+img2 = imread('nachtwacht2.jpg');
+img2 = rgb2gray(img2);
+img2 = im2single(img2);
+
+data = vl_shift(img1);
+data2 = vl_shift(img2);
+
+features = vl_ubcmatch(data, data2);
---
+++ P03/ransac.m
@@ -0,0 +1,7 @@
+function [ output ] = ransac(input)
+%RANSAC Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
