44cae4189f344b7ade8fad873fc3b0fa9994aca8
TMMeijers <t.m.meijers@Hotmail.com>
2014-12-03 15:12:40 +0100

    starting p04

---
+++ P04/main.m
@@ -0,0 +1,9 @@
+%% BEELDVERWERKEN - LAB EXERCISE 4: Decting lines by Hough Transform
+
+% COLLABORATORS:
+% A.P. van Ree (10547320)
+% T.M. Meijers (10647023)
+% DATE:
+% 8-12-2014
+
+%% Section 1: Introduction
